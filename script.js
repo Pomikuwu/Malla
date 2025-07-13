9dbe68de86aa59f80b9a11661f81a17905b46df5
Pomikuwu <3cpandachan3c@gmail.com>
2025-07-13 13:08:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll("button");
+  botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+      boton.classList.toggle("aprobado");
+    });
+  });
+});
 
